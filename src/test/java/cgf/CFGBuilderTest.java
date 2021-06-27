d9dff47da97cdf9a5f35d208cae9a6b2b2951a91
Hussani Oliveira <contato@hussani.com.br>
2021-06-27 19:33:42 -0300

    Build Sum.java
    It's a java file with only a return statement

--- src/test/java/cgf/CFGBuilderTest.java
+++ src/test/java/cgf/CFGBuilderTest.java
@@ -12,7 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CFGBuilderTest {
 
-    @Test void buildIfElseSimple() throws FileNotFoundException {
+    @Test
+    public void buildIfElseSimple() throws FileNotFoundException {
         String filename = "IfElseSimple.java";
 
         ClassLoader classLoader = getClass().getClassLoader();
@@ -24,5 +25,20 @@ class CFGBuilderTest {
         assertEquals(1, cfgList.size());
         assertEquals(5, cfgList.get(0).getEdges().size());
         assertEquals(5, cfgList.get(0).getNodes().size());
+    }
+
+    @Test
+    public void buildSum() throws FileNotFoundException {
+        String filename = "Sum.java";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+
+        CGFBuilder builder = new CGFBuilder();
+        final List<CFG> cfgList = builder.buildCFGFromFile(file);
+
+        assertEquals(1, cfgList.size());
+        assertEquals(2, cfgList.get(0).getEdges().size());
+        assertEquals(3, cfgList.get(0).getNodes().size());
     }
 }
