5b0b21751ee1865e77a82a1048c50dea402fe828
Hussani Oliveira <contato@hussani.com.br>
2021-07-04 19:03:32 -0300

    Solving constraints with ChocoSolver

--- src/main/java/constraint/ConstraintSolver.java
+++ src/main/java/constraint/ConstraintSolver.java
@@ -5,11 +5,14 @@ import cgf.CFGNode;
 import cgf.CFGSimpleNode;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.nodeTypes.NodeWithCondition;
 import com.github.javaparser.ast.type.PrimitiveType;
 import graph.CustomEdge;
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.variables.Variable;
 import org.jgrapht.GraphPath;
 
@@ -20,8 +23,10 @@ import java.util.stream.Collectors;
 
 public class ConstraintSolver {
 
+    private static final String SOLVING_TIME_LIMIT = "10s";
     private final Model model;
-    Hashtable<String, Variable> modelVariables = new Hashtable<>();
+    private final Hashtable<String, Variable> modelVariables = new Hashtable<>();
+    private final Hashtable<String, Variable> modelConstraints = new Hashtable<>();
 
     public ConstraintSolver() {
         this.model = new Model();
@@ -31,9 +36,32 @@ public class ConstraintSolver {
         Hashtable<String, Variable> variables = new Hashtable<>();
         cfg.getParameters().forEach(p -> modelVariables.put(p.getNameAsString(), this.astParameterToModelVar(p)));
 
-        final List<Expression> collect = getExpressions(graphPath);
+        final List<Expression> expressions = getExpressions(graphPath);
+
+        expressions.stream()
+                   .filter(Expression::isBinaryExpr)
+                   .map(Expression::asBinaryExpr)
+                   .forEach(this::expressionToConstraint);
+
+        Solver solver = model.getSolver();
+        solver.limitTime(SOLVING_TIME_LIMIT);
+        Solution solution = new Solution(model);
+        if (solver.solve()) {
+            solution.record();
+        }
 
         return null;
+    }
+
+    private void expressionToConstraint(BinaryExpr expression) {
+        if (!expression.getLeft().isNameExpr() || !expression.getRight().isNameExpr()) {
+            return;
+        }
+        Variable left = modelVariables.get(expression.getLeft().asNameExpr().getNameAsString());
+        Variable right =  modelVariables.get(expression.getRight().asNameExpr().getNameAsString());
+
+        model.arithm(left.asIntVar(), expression.getOperator().asString(), right.asIntVar()).post();
+
     }
 
     private List<Expression> getExpressions(GraphPath<CFGNode, CustomEdge> graphPath) {
