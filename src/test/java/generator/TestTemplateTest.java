5de7f9248c4c49d0b983a0d035be87c276ca1c27
Hussani Oliveira <contato@hussani.com.br>
2021-07-06 03:43:56 -0300

    Adding more complex if chain

--- src/test/java/generator/TestTemplateTest.java
+++ src/test/java/generator/TestTemplateTest.java
@@ -45,4 +45,29 @@ class TestTemplateTest {
 
         assertEquals(2, cu.findAll(MethodDeclaration.class).size());
     }
+
+    @Test
+    public void testGenerateComplexTestFile() throws FileNotFoundException {
+        String filename = "If4Paths.java";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+
+        CFGBuilder builder = new CFGBuilder();
+        final CFG cfg = builder.buildCFGFromFile(file).get(0);
+
+        PathGenerator generator = new PathGenerator();
+        final List<GraphPath<CFGNode, CustomEdge>> allPathsFromCFG = generator.getAllPathsFromCFG(cfg);
+
+        final List<TestSolution> testSolutions = allPathsFromCFG.stream().map(path -> {
+            ConstraintSolver solver = new ConstraintSolver(cfg, path);
+            return new TestSolution(cfg, solver.solveConstraints(), solver.resolveExpectedReturn());
+        }).collect(Collectors.toList());
+
+        TestTemplate template = new TestTemplate();
+
+        CompilationUnit cu = template.createCompilationUnit(cfg.getClassName(), testSolutions);
+
+        assertEquals(4, cu.findAll(MethodDeclaration.class).size());
+    }
 }
