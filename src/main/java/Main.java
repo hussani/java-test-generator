4cec8c43e0e33a600da41dba07e6bf85bbb3ab6a
Hussani Oliveira <contato@hussani.com.br>
2021-07-06 20:40:54 -0300

    Adding main application

---
+++ src/main/java/Main.java
@@ -0,0 +1,69 @@
+import cgf.CFG;
+import cgf.CFGBuilder;
+import com.github.javaparser.ast.CompilationUnit;
+import constraint.ConstraintSolver;
+import constraint.TestSolution;
+import generator.TestTemplate;
+import path.PathGenerator;
+import support.Tuple2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static final String GENERATED_TESTS_FOLDER = "generated-tests";
+
+    public static void main(String[] args) throws IOException {
+        String filename = args[0];
+
+        File file = new File(filename);
+
+        PathGenerator generator = new PathGenerator();
+        CFGBuilder builder = new CFGBuilder();
+        final List<CFG> cfgList = builder.buildCFGFromFile(file);
+
+        if (cfgList.isEmpty()) {
+            throw new RuntimeException("No methods was found in file");
+        }
+
+        final String className = cfgList.get(0).getClassName();
+
+        String output;
+        if (args.length > 1) {
+            output = args[1];
+        } else {
+            output = className + "Test.java";
+        }
+
+        final List<TestSolution> testSolutions = cfgList.stream()
+                                                        .map(cfg -> new Tuple2<>(cfg,
+                                                                generator.getAllPathsFromCFG(cfg)))
+                                                        .map(tuple -> tuple.getT2().stream().map(path -> {
+                                                            ConstraintSolver solver =
+                                                                    new ConstraintSolver(tuple.getT1(), path);
+                                                            return new TestSolution(tuple.getT1(),
+                                                                    solver.solveConstraints(),
+                                                                    solver.resolveExpectedReturn());
+                                                        }).collect(Collectors.toList()))
+                                                        .flatMap(List::stream)
+                                                        .collect(Collectors.toList());
+
+        TestTemplate template = new TestTemplate();
+
+        CompilationUnit cu = template.createCompilationUnit(className, testSolutions);
+
+        Files.createDirectories(Paths.get(GENERATED_TESTS_FOLDER));
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_TESTS_FOLDER + "/ " + output));
+        writer.write(cu.toString());
+        writer.close();
+
+    }
+}
